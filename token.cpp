bea3b4a404b054315f0e8e7bde8efe9d68e83c96
enumivo-core <46376384+enumivo-core@users.noreply.github.com>
2019-11-01 09:48:26 +0800

    Add files via upload

---
+++ token.cpp
@@ -0,0 +1,227 @@
+#include "token.hpp"
+#include <eosio/system.hpp>
+
+namespace eosio {
+
+void token::create( const name&   issuer,
+                    const asset&  maximum_supply )
+{
+    require_auth( get_self() );
+
+    auto sym = maximum_supply.symbol;
+    check( sym.is_valid(), "invalid symbol name" );
+    check( maximum_supply.is_valid(), "invalid supply");
+    check( maximum_supply.amount > 0, "max-supply must be positive");
+
+    stats statstable( get_self(), sym.code().raw() );
+    auto existing = statstable.find( sym.code().raw() );
+    check( existing == statstable.end(), "token with symbol already exists" );
+
+    statstable.emplace( get_self(), [&]( auto& s ) {
+       s.supply.symbol = maximum_supply.symbol;
+       s.max_supply    = maximum_supply;
+       s.issuer        = issuer;
+    });
+}
+
+
+void token::issue( const name& to, const asset& quantity, const string& memo )
+{
+    auto sym = quantity.symbol;
+    check( sym.is_valid(), "invalid symbol name" );
+    check( memo.size() <= 256, "memo has more than 256 bytes" );
+
+    stats statstable( get_self(), sym.code().raw() );
+    auto existing = statstable.find( sym.code().raw() );
+    check( existing != statstable.end(), "token with symbol does not exist, create token before issue" );
+    const auto& st = *existing;
+    check( to == st.issuer, "tokens can only be issued to issuer account" );
+
+    require_auth( st.issuer );
+    check( quantity.is_valid(), "invalid quantity" );
+    check( quantity.amount > 0, "must issue positive quantity" );
+
+    check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+    check( quantity.amount <= st.max_supply.amount - st.supply.amount, "quantity exceeds available supply");
+
+    statstable.modify( st, same_payer, [&]( auto& s ) {
+       s.supply += quantity;
+    });
+
+    add_balance( st.issuer, quantity, st.issuer );
+}
+
+void token::retire( const asset& quantity, const string& memo )
+{
+    auto sym = quantity.symbol;
+    check( sym.is_valid(), "invalid symbol name" );
+    check( memo.size() <= 256, "memo has more than 256 bytes" );
+
+    stats statstable( get_self(), sym.code().raw() );
+    auto existing = statstable.find( sym.code().raw() );
+    check( existing != statstable.end(), "token with symbol does not exist" );
+    const auto& st = *existing;
+
+    require_auth( st.issuer );
+    check( quantity.is_valid(), "invalid quantity" );
+    check( quantity.amount > 0, "must retire positive quantity" );
+
+    check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+
+    statstable.modify( st, same_payer, [&]( auto& s ) {
+       s.supply -= quantity;
+    });
+
+    sub_balance( st.issuer, quantity );
+}
+
+void token::transfer( const name&    from,
+                      const name&    to,
+                      const asset&   quantity,
+                      const string&  memo )
+{
+    check( from != to, "cannot transfer to self" );
+    require_auth( from );
+    check( is_account( to ), "to account does not exist");
+    auto sym = quantity.symbol.code();
+    stats statstable( get_self(), sym.raw() );
+    const auto& st = statstable.get( sym.raw() );
+
+    require_recipient( from );
+    require_recipient( to );
+
+    check( quantity.is_valid(), "invalid quantity" );
+    check( quantity.amount > 0, "must transfer positive quantity" );
+    check( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+    check( memo.size() <= 256, "memo has more than 256 bytes" );
+
+    auto payer = has_auth( to ) ? to : from;
+
+    sub_balance( from, quantity );
+    add_balance( to, quantity, payer );
+}
+
+void token::sub_balance( const name& owner, const asset& value ) {
+   accounts from_acnts( get_self(), owner.value );
+
+   const auto& from = from_acnts.get( value.symbol.code().raw(), "no balance object found" );
+   check( from.balance.amount >= value.amount, "overdrawn balance" );
+
+   from_acnts.modify( from, owner, [&]( auto& a ) {
+         a.balance -= value;
+      });
+}
+
+void token::add_balance( const name& owner, const asset& value, const name& ram_payer )
+{
+   accounts to_acnts( get_self(), owner.value );
+   auto to = to_acnts.find( value.symbol.code().raw() );
+   if( to == to_acnts.end() ) {
+      to_acnts.emplace( ram_payer, [&]( auto& a ){
+        a.balance = value;
+      });
+   } else {
+      to_acnts.modify( to, same_payer, [&]( auto& a ) {
+        a.balance += value;
+      });
+   }
+}
+
+void token::open( const name& owner, const symbol& symbol, const name& ram_payer )
+{
+   require_auth( ram_payer );
+
+   check( is_account( owner ), "owner account does not exist" );
+
+   auto sym_code_raw = symbol.code().raw();
+   stats statstable( get_self(), sym_code_raw );
+   const auto& st = statstable.get( sym_code_raw, "symbol does not exist" );
+   check( st.supply.symbol == symbol, "symbol precision mismatch" );
+
+   accounts acnts( get_self(), owner.value );
+   auto it = acnts.find( sym_code_raw );
+   if( it == acnts.end() ) {
+      acnts.emplace( ram_payer, [&]( auto& a ){
+        a.balance = asset{0, symbol};
+      });
+   }
+}
+
+void token::close( const name& owner, const symbol& symbol )
+{
+   require_auth( owner );
+   accounts acnts( get_self(), owner.value );
+   auto it = acnts.find( symbol.code().raw() );
+   check( it != acnts.end(), "Balance row already deleted or never existed. Action won't have any effect." );
+   check( it->balance.amount == 0, "Cannot close because the balance is not zero." );
+   acnts.erase( it );
+}
+
+void token::claim(name from, name to, eosio::asset quantity, std::string memo)
+{
+   if (to != get_self() || from == get_self()) 
+         return;
+
+   action{
+         permission_level{get_self(), "active"_n},
+         "eosio.token"_n,
+         "transfer"_n,
+         std::make_tuple(get_self(), from, quantity, std::string("Refund EOS"))
+   }.send();              
+
+   int elapsed = current_time_point().sec_since_epoch();
+
+   if (elapsed > 1572595200) {
+      asset supply = eosio::token::get_supply(get_self(), symbol_code("EIDOS"));    
+      int64_t expected = (elapsed - 1572595200) * 25;
+
+      asset balance = eosio::token::get_balance(get_self(), get_self(), symbol_code("EIDOS"));
+
+      if (expected > 1000000000)
+         expected = 1000000000;
+
+      asset expected_supply = asset(expected, symbol("EIDOS", 4));
+      expected_supply *= 10000;
+
+      if (supply < expected_supply){
+         asset claim = expected_supply-supply;
+
+         action{
+            permission_level{get_self(), "active"_n}, 
+            get_self(),
+            "issue"_n,
+            std::make_tuple(get_self(), claim, std::string("Issue EIDOS"))
+         }.send();
+
+         balance += claim;
+
+         claim = claim / 5;
+
+         action{
+            permission_level{get_self(), "active"_n},
+            get_self(),
+            "transfer"_n,
+            std::make_tuple(get_self(), "eidosoneteam"_n, claim, std::string("Send to EIDOS Team."))
+         }.send();              
+
+         balance -= claim;
+
+      }
+
+      if (balance > asset(0, symbol("EIDOS", 4))) {
+         if (balance <= asset(10000, symbol("EIDOS", 4)))
+            balance = asset(1, symbol("EIDOS", 4));
+         else   
+            balance /= 10000;
+
+         action{
+            permission_level{get_self(), "active"_n},
+            get_self(),
+            "transfer"_n,
+            std::make_tuple(get_self(), from, balance, std::string("Airdrop EIDOS"))
+         }.send();              
+      }
+   }  
+}    
+
+} 
